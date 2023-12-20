248432ba717c2f1f9b18061b9cc34de28a30648f
freedoom16 <nmaaa007@gmail.com>
2023-12-20 17:00:58 +0300

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,147 +1,114 @@
-// import functionalities
-import React, { useEffect, useState } from "react";
-import { PublicKey, Transaction } from "@solana/web3.js";
+import React, { useState } from "react";
+import {
+  Connection,
+  Keypair,
+  clusterApiUrl,
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+  Transaction,
+  sendAndConfirmTransaction,
+  PublicKey,
+} from "@solana/web3.js";
 
-// create types
-type DisplayEncoding = "utf8" | "hex";
+const App = () => {
+  const [account, setAccount] = useState<any>(null);
+  const [connectedWallet, setConnectedWallet] = useState(null);
 
-type PhantomEvent = "disconnect" | "connect" | "accountChanged";
-type PhantomRequestMethod =
-  | "connect"
-  | "disconnect"
-  | "signTransaction"
-  | "signAllTransactions"
-  | "signMessage";
+  const createNewAccount = async () => {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-interface ConnectOpts {
-  onlyIfTrusted: boolean;
-}
+    // Generate a new keypair
+    const newAccount: any = Keypair.generate();
 
-// create a provider interface (hint: think of this as an object) to store the Phantom Provider
-interface PhantomProvider {
-  publicKey: PublicKey | null;
-  isConnected: boolean | null;
-  signTransaction: (transaction: Transaction) => Promise<Transaction>;
-  signAllTransactions: (transactions: Transaction[]) => Promise<Transaction[]>;
-  signMessage: (
-    message: Uint8Array | string,
-    display?: DisplayEncoding
-  ) => Promise<any>;
-  connect: (opts?: Partial<ConnectOpts>) => Promise<{ publicKey: PublicKey }>;
-  disconnect: () => Promise<void>; // New disconnect method
-  on: (event: PhantomEvent, handler: (args: any) => void) => void;
-  request: (method: PhantomRequestMethod, params: any) => Promise<unknown>;
-}
+    // Airdrop 2 SOL to the new account
+    await connection.requestAirdrop(newAccount.publicKey, 2 * LAMPORTS_PER_SOL);
 
-/**
- * @description gets Phantom provider, if it exists
- */
-const getProvider = (): PhantomProvider | undefined => {
-  if ("solana" in window) {
-    // @ts-ignore
-    const provider = window.solana as any;
-    if (provider.isPhantom) return provider as PhantomProvider;
-  }
-};
+    console.log("New account created:", newAccount.publicKey.toBase58());
+    setAccount(newAccount);
+  };
 
-function App() {
-  // create state variable for the provider
-  const [provider, setProvider] = useState<PhantomProvider | undefined>(
-    undefined
-  );
+  const connectToPhantomWallet = async () => {
+    const { solana }: any = window;
 
-  // create state variable for the wallet key
-  const [walletKey, setWalletKey] = useState<string | undefined>(undefined);
-
-  // this is the function that runs whenever the component updates (e.g., render, refresh)
-  useEffect(() => {
-    const provider = getProvider();
-
-    // if the phantom provider exists, set this as the provider
-    if (provider) setProvider(provider);
-    else setProvider(undefined);
-  }, []);
-
-  /**
-   * @description prompts the user to connect wallet if it exists.
-   * This function is called when the connect wallet button is clicked
-   */
-  const connectWallet = async () => {
-    // @ts-ignore
-    const { solana } = window;
-
-    // checks if phantom wallet exists
     if (solana) {
       try {
-        // connects wallet and returns response which includes the wallet public key
         const response = await solana.connect();
-        console.log("wallet account ", response.publicKey.toString());
-        // update walletKey to be the public key
-        setWalletKey(response.publicKey.toString());
+        console.log("Connected to Phantom Wallet");
+        setConnectedWallet(response.publicKey.toString());
       } catch (err) {
-        // { code: 4001, message: 'User rejected the request.' }
+        console.error("Error connecting to Phantom Wallet", err);
       }
     }
   };
 
-  /**
-   * @description disconnects the current Phantom wallet instance
-   */
-  const disconnectWallet = async () => {
-    // @ts-ignore
-    const { solana } = window;
+  // const transferToNewWallet = async () => {
+  //   const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-    // checks if phantom wallet exists
-    if (solana) {
-      try {
-        // disconnect the wallet
-        await solana.disconnect();
-        // clear the walletKey state
-        setWalletKey(undefined);
-      } catch (err) {
-        console.error("Error disconnecting wallet", err);
-      }
+  //   if (!account || !connectedWallet) {
+  //     console.error("Account or connected wallet is not set.");
+  //     return;
+  //   }
+
+  //   // Create a transaction to transfer 1 SOL
+  //   const transaction = new Transaction().add(
+  //     SystemProgram.transfer({
+  //       fromPubkey: account.publicKey,
+  //       toPubkey: new Keypair().publicKey, // Placeholder for the connected wallet
+  //       lamports: LAMPORTS_PER_SOL / 100, // 1 SOL
+  //     })
+  //   );
+
+  //   // Sign and confirm the transaction
+  //   const signature = await sendAndConfirmTransaction(connection, transaction, [
+  //     account,
+  //   ]);
+
+  //   console.log("Transfer completed. Signature:", signature);
+  // };
+
+  const transferToNewWallet = async () => {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+    if (!account || !connectedWallet) {
+      console.error("Account or connected wallet is not set.");
+      return;
     }
+
+    // Encode the transfer instruction
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: account.publicKey,
+        toPubkey: new PublicKey(connectedWallet),
+        lamports: LAMPORTS_PER_SOL / 100, // 1 SOL
+      })
+    );
+
+    // Sign and confirm the transaction
+    const signature = await sendAndConfirmTransaction(connection, transaction, [
+      account,
+    ]);
+
+    console.log("Transfer completed. Signature:", signature);
   };
 
-  // HTML code for the app
   return (
-    <div className="App min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <div className="flex ">
-        <h2 className="text-2xl font-bold p-4 rounded bg-blue-500 text-white">
-          Connect to Phantom Wallet
-        </h2>
-        {provider && walletKey && (
-          <button
-            className="text-xl font-bold p-4 rounded bg-red-500 text-white float-right"
-            onClick={disconnectWallet}
-          >
-            Disconnect
-          </button>
-        )}
-        {provider && !walletKey && (
-          <button
-            className="text-xl font-bold p-4 rounded bg-green-500 text-white"
-            onClick={connectWallet}
-          >
-            Connect Wallet
-          </button>
-        )}
-        {provider && walletKey && (
-          <p className="mt-4">Connected account: {walletKey}</p>
+    <div className="bg-blue-300">
+      <button className="" onClick={createNewAccount}>
+        Create New Solana Account
+      </button>
+      <button onClick={connectToPhantomWallet}>
+        Connect to Phantom Wallet
+      </button>
+      <button onClick={transferToNewWallet}>Transfer to New Wallet</button>
+
+      <div>
+        {account && <p>New account created: {account.publicKey.toBase58()}</p>}
+        {connectedWallet && (
+          <p>Connected to Phantom Wallet: {connectedWallet}</p>
         )}
       </div>
-
-      {!provider && (
-        <p className="mt-8">
-          No provider found. Install{" "}
-          <a href="https://phantom.app/" className="text-blue-500">
-            Phantom Browser extension
-          </a>
-        </p>
-      )}
     </div>
   );
-}
+};
 
 export default App;
